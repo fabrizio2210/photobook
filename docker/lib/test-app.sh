6c98d77be7a81ee9f5c62cf00590cb5675cfff3b
Fabrizio W <fabrizio2210@gamil.com>
2022-02-26 15:15:22 +0100

    Turn down containers after test

--- docker/lib/test-app.sh
+++ docker/lib/test-app.sh
@@ -3,3 +3,4 @@
 # Supposing to deploy on x86_64 architecture
 docker build -t fabrizio2210/photobook-backend-dev -f docker/x86_64/Dockerfile-backend-dev .
 docker-compose -f docker/lib/stack-test.yml run flask
+docker-compose -f docker/lib/stack-test.yml down
