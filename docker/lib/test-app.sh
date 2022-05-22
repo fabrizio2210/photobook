ec0551a24336685ecc0ff42c578caa162702b9e3
Fabrizio W <fabrizio2210@gamil.com>
2022-05-22 18:48:22 +0200

    Fix assignement

--- docker/lib/test-app.sh
+++ docker/lib/test-app.sh
@@ -3,7 +3,7 @@
 if [ "$(uname -m)" == "x86_64" ]; then
   arch="x86_64"
 else
-  arch"armv7hf"
+  arch="armv7hf"
 fi
 
 docker build -t fabrizio2210/photobook-backend:${arch} -f docker/x86_64/Dockerfile-backend .
