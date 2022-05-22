976c4df80451c0dfcf9393d479c266cc83503ebb
Fabrizio W <fabrizio2210@gamil.com>
2022-05-22 18:35:10 +0200

    Continous delivery of the image

--- CICD.sh
+++ CICD.sh
@@ -6,12 +6,6 @@ set -x
 #############
 # Environment
 
-registryCredential='docker-login'
-buildDir='/tmp/build'
-debPackageStash='deb'
-venvPackageStash='venv'
-
-runRepository="$(mktemp -d)"
 workspace=$(dirname $0)
 if ! echo $workspace |  grep "^/" ;  then
   workspace="$(pwd)/$workspace"
@@ -19,7 +13,41 @@ fi
 repository='/tmp'
 changedFiles="$(git diff --name-only HEAD^1 HEAD)"
 
-echo $(pwd)
+if [ $(uname -m) = "x86_64" ] ; then
+  arch="x86_64"
+else
+  arch="armv7hf"
+fi
+
+################
+# Login creation
+
+mkdir -p ~/.docker/
+
+if [ -z "$DOCKER_LOGIN" ] ; then
+	echo "Docker login not found in the environment, set DOCKER_LOGIN"
+else
+  cat << EOF > ~/.docker/config.json
+{
+  "experimental": "enabled",
+        "auths": {
+                "https://index.docker.io/v1/": {
+                        "auth": "$DOCKER_LOGIN"
+                }
+        },
+        "HttpHeaders": {
+                "User-Agent": "Docker-Client/17.12.1-ce (linux)"
+        }
+}
+EOF
+fi
+
+######
+# Test
+
 docker/lib/test-app.sh
-echo $?
-exit 0
+
+######
+# Push
+
+docker push -t fabrizio2210/photobook-backend:${arch}
