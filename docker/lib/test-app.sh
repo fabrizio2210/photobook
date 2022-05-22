f0b851efc74410f68775ce88c23fbdf9a7b07201
Fabrizio W <fabrizio2210@gamil.com>
2022-05-22 18:16:38 +0200

    Using self-containing image for testing.

--- docker/lib/test-app.sh
+++ docker/lib/test-app.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -xeu
 
 echo $(pwd)
-docker build -t fabrizio2210/photobook-backend-dev -f docker/x86_64/Dockerfile-backend-dev .
+docker build -t fabrizio2210/photobook-backend -f docker/x86_64/Dockerfile-backend .
 if [ "$(uname -m)" == "x86_64" ]; then
   stack="docker/lib/stack-test-x86_64.yml"
 else
   stack="docker/lib/stack-test-armv7hf.yml"
 fi
 
-docker-compose -f ${stack} run -v $(pwd)/src:/opt/web flask
+docker-compose -f ${stack} run flask
 docker-compose -f ${stack} down
