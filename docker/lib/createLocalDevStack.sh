0bc9f8d62dfc3b5ef9a3e67e90b7c50d89fd502b
Fabrizio W <fabrizio2210@gamil.com>
2023-01-29 17:33:16 +0000

    updating docker compose command

--- docker/lib/createLocalDevStack.sh
+++ docker/lib/createLocalDevStack.sh
@@ -3,4 +3,4 @@
 # Supposing to deploy on x86_64 architecture
 docker build -t fabrizio2210/photobook-backend-dev -f docker/x86_64/Dockerfile-backend-dev .
 docker build -t fabrizio2210/photobook-frontend-dev -f docker/x86_64/Dockerfile-frontend-dev .
-docker-compose -f docker/lib/stack-dev.yml  up
+docker compose -f docker/lib/stack-dev.yml  up
