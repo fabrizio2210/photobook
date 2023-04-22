66ee16ec174794dc2e2202be42a5478d799aa44b
Fabrizio W <fabrizio2210@gamil.com>
2023-04-22 14:01:22 +0100

    New container every time

--- docker/lib/createLocalDevStack.sh
+++ docker/lib/createLocalDevStack.sh
@@ -5,4 +5,4 @@ docker build -t fabrizio2210/photobook-backend-dev -f docker/x86_64/Dockerfile-b
 docker build -t fabrizio2210/photobook-frontend-dev -f docker/x86_64/Dockerfile-frontend-dev .
 docker build -t fabrizio2210/photobook-worker-dev -f docker/x86_64/Dockerfile-worker-dev .
 docker build -t fabrizio2210/photobook-sse-dev -f docker/x86_64/Dockerfile-sse-dev .
-docker compose -f docker/lib/stack-dev.yml --env-file="/home/fabrizio/.docker/photobook-dev.env" up
+docker compose -f docker/lib/stack-dev.yml --env-file="/home/fabrizio/.docker/photobook-dev.env" up --force-recreate --remove-orphans --renew-anon-volumes
