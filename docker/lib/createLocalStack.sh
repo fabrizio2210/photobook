eb16c4854de64b93e48d15c38f5ede5d3f9a004e
Fabrizio W <fabrizio2210@gamil.com>
2023-05-01 14:36:47 +0100

    Productionization

--- docker/lib/createLocalStack.sh
+++ docker/lib/createLocalStack.sh
@@ -3,4 +3,7 @@
 # Supposing to deploy on x86_64 architecture
 docker build -t fabrizio2210/photobook-backend -f docker/x86_64/Dockerfile-backend .
 docker build -t fabrizio2210/photobook-frontend -f docker/x86_64/Dockerfile-frontend .
-docker-compose -f docker/lib/stack.yml  up
+docker build -t fabrizio2210/photobook-worker -f docker/x86_64/Dockerfile-worker .
+docker build -t fabrizio2210/photobook-api -f docker/x86_64/Dockerfile-api .
+docker build -t fabrizio2210/photobook-sse -f docker/x86_64/Dockerfile-sse .
+docker compose -f docker/lib/stack.yml  up
