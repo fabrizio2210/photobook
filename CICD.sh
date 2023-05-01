d1194de5788ffd4df66559ece2fb131fdca11a0f
Fabrizio W <fabrizio2210@gamil.com>
2023-05-01 14:38:24 +0100

    In CICD

--- CICD.sh
+++ CICD.sh
@@ -49,6 +49,9 @@ fi
 
 docker build -t fabrizio2210/photobook-frontend:${arch} -f docker/x86_64/Dockerfile-frontend .
 docker build -t fabrizio2210/photobook-backend:${arch} -f docker/x86_64/Dockerfile-backend .
+docker build -t fabrizio2210/photobook-sse:${arch} -f docker/x86_64/Dockerfile-sse .
+docker build -t fabrizio2210/photobook-worker:${arch} -f docker/x86_64/Dockerfile-worker .
+docker build -t fabrizio2210/photobook-api:${arch} -f docker/x86_64/Dockerfile-api .
 
 ######
 # Test
@@ -60,3 +63,6 @@ docker/lib/test-app.sh
 
 docker push fabrizio2210/photobook-frontend:${arch}
 docker push fabrizio2210/photobook-backend:${arch}
+docker push fabrizio2210/photobook-sse:${arch}
+docker push fabrizio2210/photobook-worker:${arch}
+docker push fabrizio2210/photobook-api:${arch}
