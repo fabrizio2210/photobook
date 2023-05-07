4e33cd34339ff464b894b7bf200793339d2d9c28
Fabrizio W <fabrizio2210@gamil.com>
2023-05-07 16:52:56 +0100

    Testing with mocked response

--- src/go/api/api_test.go
+++ src/go/api/api_test.go
@@ -8,9 +8,15 @@ import (
 	"testing"
 
   "Api/responses"
+  "Api/controllers"
+  "Api/models"
+  "Api/db"
 
 	"github.com/stretchr/testify/assert"
+  "go.mongodb.org/mongo-driver/bson"
+  "go.mongodb.org/mongo-driver/mongo/integration/mtest"
 )
+
 
 func TestUidRoute(t *testing.T) {
 	router := setupRouter()
@@ -26,3 +32,53 @@ func TestUidRoute(t *testing.T) {
 
 }
 
+
+func TestEventRoute(t *testing.T) {
+	router := setupRouter()
+  mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+  defer mt.Close()
+
+  mt.Run("test", func(mt *mtest.T) {
+    db.DB = mt.Client
+    controllers.EventCollection = mt.Coll
+    want := models.PhotoEvent{
+      Author:"author",
+      Author_id:"abc-123-abc",
+      Description:"A description",
+      Event:"creation",
+      Id:"abc-123-123",
+      Location:"/static/resized/abc-123.jpg",
+      Order:1,
+      Photo_id:"abc-123",
+      Timestamp:1,
+    }
+    first := mtest.CreateCursorResponse(1, "test.trainers", mtest.FirstBatch, bson.D{
+      {Key: "Author", Value: want.Author},
+      {Key: "Author_id", Value: want.Author_id},
+      {Key: "Description", Value: want.Description},
+      {Key: "Event", Value: want.Event},
+      {Key: "Id", Value: want.Id},
+      {Key: "Order", Value: want.Order},
+      {Key: "Photo_id", Value: want.Photo_id},
+      {Key: "Timestamp", Value: want.Timestamp},
+    })
+    killCursors := mtest.CreateCursorResponse(0, "test.trainers", mtest.NextBatch)
+    mt.AddMockResponses(first, killCursors)
+
+
+    w := httptest.NewRecorder()
+    req, _ := http.NewRequest("GET", "/api/photo/abc-123", nil)
+    router.ServeHTTP(w, req)
+
+    assert.Equal(t, 200, w.Code)
+    d := json.NewDecoder(w.Body)
+    d.UseNumber()
+    var res responses.Response
+    d.Decode(&res)
+    jsonData, _ := json.Marshal(res.Data["event"])
+    var event models.PhotoEvent
+    json.Unmarshal(jsonData, &event)
+    assert.EqualValues(t, want, event)
+  })
+
+}
