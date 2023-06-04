317e587ab718ec113f0793ee5507ac7dfc937b61
Fabrizio W <fabrizio2210@gamil.com>
2023-06-04 15:08:00 +0100

    Variable docker compose

--- docker/lib/test-app.sh
+++ docker/lib/test-app.sh
@@ -13,5 +13,8 @@ else
   stack="docker/lib/stack-test-armv7hf.yml"
 fi
 
-docker compose -f ${stack} run api
-docker compose -f ${stack} down
+docker_compose="docker-compose"
+docker compose version && docker_compose="docker compose"
+
+${docker_compose} -f ${stack} run api
+${docker_compose} -f ${stack} down
