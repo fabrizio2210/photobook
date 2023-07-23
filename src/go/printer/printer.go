dc05a334e8f5713cebdd83951b08bd4c08bfa185
Fabrizio W <fabrizio2210@gamil.com>
2023-07-23 15:26:27 +0100

    First prototype for Printer

--- src/go/printer/printer.go
+++ src/go/printer/printer.go
@@ -14,11 +14,51 @@ import (
 var ctx = context.Background()
 var EventCollection *mongo.Collection
 
-
-func convertEventsToLayout(events []models.PhotoEvent, layout [][2]models.PhotoEvent) {
-  for _, event := range events {
-   log.Printf("%+v", event) 
+func removePhoto(photos []models.PhotoEvent, event models.PhotoEvent) []models.PhotoEvent {
+  for i := range photos {
+    if photos[i].Photo_id == event.Photo_id {
+      return append(photos[:i], photos[i+1:]...)
+    }
   }
+  return photos
+}
+
+func substitutePhoto(photos []models.PhotoEvent, event models.PhotoEvent) []models.PhotoEvent {
+  for i := range photos {
+    if photos[i].Photo_id == event.Photo_id {
+      photos[i] = event
+      return photos
+    }
+  }
+  return photos
+}
+  
+
+func convertEventsToLayout(events []models.PhotoEvent) []*[2]models.PhotoEvent {
+  var photos []models.PhotoEvent
+  layout := []*[2]models.PhotoEvent{}
+
+  for _, event := range events {
+    switch event.Event {
+    case "creation":
+     photos = append(photos, event)
+    case "deletion":
+     photos = removePhoto(photos, event)
+    case "edit":
+     photos = substitutePhoto(photos, event)
+    }
+  }
+
+  var page *[2]models.PhotoEvent
+  for i, event := range photos {
+    log.Printf("%+v", event) 
+    if i%2 == 0 {
+      page = &[2]models.PhotoEvent{}
+      layout = append(layout, page)
+    }
+    page[i%2] = event
+  }
+  return layout
 }
 
 func main() {
@@ -35,22 +75,10 @@ func main() {
     panic(err)
   }
 
-  layout := [][2]models.PhotoEvent{}
 
-  convertEventsToLayout(events, layout)
-
-  cs, err := EventCollection.Watch(ctx, mongo.Pipeline{}, options.ChangeStream().SetFullDocument(options.UpdateLookup))
-  if err != nil {
-    panic(err)
-  }
-  defer cs.Close(ctx)
-
-  for {
-    if ok := cs.Next(ctx) ; ok {
-      next := cs.Current
-      log.Printf("%v", next)
-      // convertEventsToLayout([]models.PhotoEvent{next["fullDocument"]}, layout)
-    }
+  layout := convertEventsToLayout(events)
+  log.Printf("Layout:%+v", layout)
+  for _, page := range layout{
+    log.Printf("Page:%+v", *page)
   }
 }
-
