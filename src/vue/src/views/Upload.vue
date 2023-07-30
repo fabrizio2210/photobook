c238a907dcb4c8179bbef62d98dc1cbeb5c30b2f
Fabrizio W <fabrizio2210@gamil.com>
2023-07-30 14:50:14 +0100

    Increase to 15MB the max picture size

--- src/vue/src/views/Upload.vue
+++ src/vue/src/views/Upload.vue
@@ -8,7 +8,7 @@
         extensions="gif,jpg,jpeg,png,webp"
         accept="image/png,image/gif,image/jpeg,image/webp"
         :multiple="false"
-        :size="1024 * 1024 * 10"
+        :size="1024 * 1024 * 15"
         ref="upload"
         @input-filter="inputFilter"
         v-model="files"
