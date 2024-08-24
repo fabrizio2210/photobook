7cc81b64d591ae5e41a545b7eea00f550d54bf20
Fabrizio W <fabrizio2210@gamil.com>
2024-08-24 16:30:02 +0100

    Remove old backend image

--- docker/lib/createLocalDevStack.sh
+++ docker/lib/createLocalDevStack.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Supposing to deploy on x86_64 architecture
-docker build -t fabrizio2210/photobook-backend-dev -f docker/x86_64/Dockerfile-backend-dev .
 docker build -t fabrizio2210/photobook-frontend-dev -f docker/x86_64/Dockerfile-frontend-dev .
 docker build -t fabrizio2210/photobook-worker-dev -f docker/x86_64/Dockerfile-worker-dev .
 docker build -t fabrizio2210/photobook-api-dev -f docker/x86_64/Dockerfile-api-dev .
